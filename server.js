4bd174554d19a60290ba0b49011850f53e2bd87f
Konstantin Popov <popov.k.g@gmail.com>
2022-09-26 13:15:51 +0300

    added dotenv

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const app = require("./app");
 
+const { connect } = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const { MONGO_URL } = process.env;
+console.log(MONGO_URL);
+
+connect(MONGO_URL, { dbName: "db-contacts" })
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.error(error.message));
+
 app.listen(5000, () => {
   console.log("Server running. Use our API on port: 5000");
 });
